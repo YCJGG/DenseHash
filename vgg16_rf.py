a9c7e59a01c1a36a74f2edd704dd45ebe2f2f879
kfxw <zhen.wei@hotmail.com>
2018-01-12 20:48:01 +0800

    20180112 first commit

---
+++ vgg16_rf.py
@@ -0,0 +1,78 @@
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+
+class VGG(nn.Module):
+    def __init__(self, num_classes = 1000):
+        super(VGG, self).__init__()
+        self.features = nn.Sequential(
+
+            nn.Conv2d(3,64,3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(64,64,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            
+            nn.Conv2d(64,128,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+            
+            nn.Conv2d(128,256,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256,256,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2),
+
+
+            nn.Conv2d(256,512,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=1, padding = 1),
+
+            nn.Conv2d(512,512,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512,3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512,512,3, padding=2,dilation=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=1,padding = 1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(512,4096,7,padding = 3),
+            nn.ReLU (True),
+            nn.Dropout(),
+            nn.Conv2d(4096,4096,1),
+            nn.ReLU(True),
+            nn.Dropout(),
+            #nn.Conv2d(4096,num_classes,1),
+            nn.AvgPool2d(28),
+            #nn.Linear(1*1*num_classes,num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        #x = x.view(x.size(0),-1)
+        x = self.classifier(x)
+        return x 
+
+   
+
+
+
+
+
+
+
+            
+    
+             
+       
+
+      
